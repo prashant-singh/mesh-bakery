df60c1a445856945ee411e7d6b2b1cd95ed6a933
Prashant Singh <prashant19194@gmail.com>
2026-06-24 12:56:49 +0530

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,8 @@
 export const BASE_PATH =
   process.env.NEXT_PUBLIC_BASE_PATH || '';
+
+
+export function withBasePath(path: string) {
+  if (!path.startsWith('/')) return path;
+  return `${basePath}${path}`;
+}
